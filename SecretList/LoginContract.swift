eb2b599b741f478cb33ae0e47a954fbff6ca1998
Kittinun Vantasin <kittinun.f@gmail.com>
2016-06-25 19:23:42 +0700

    Apply MVVM for Login page

---
+++ SecretList/LoginContract.swift
@@ -0,0 +1,29 @@
+//
+//  LoginContract.swift
+//  SecretList
+//
+//  Created by Kittinun Vantasin on 6/25/16.
+//  Copyright © 2016 Kiattisak Anoochitarom. All rights reserved.
+//
+
+import Foundation
+
+protocol LoginViewType: class {
+  //intent
+  var username: Flow<String> { get }
+  var password: Flow<String> { get }
+  var loginDidTap: Flow<Void> { get }
+
+  //action
+  func handleLoginSuccess()
+  func handleLoginFailure(error: NSError)
+  
+}
+
+protocol LoginViewModelType {
+  
+  var isCredentialValid: Flow<Bool> { get }
+  
+  func login(username: String, password: String)
+  
+}
--- SecretList/LoginViewController.swift
+++ SecretList/LoginViewController.swift
@@ -8,74 +8,62 @@
 
 import UIKit
 
-private let MinimumPasswordCharacters = 6
 private let SecretListSegueIdentifier = "SecretListSegue"
 
-class LoginViewController: UIViewController {
-    @IBOutlet var emailField: UITextField!
-    @IBOutlet var passwordField: UITextField!
-    @IBOutlet var loginButton: UIButton!
+class LoginViewController : UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupView()
+  @IBOutlet var emailField: UITextField!
+  @IBOutlet var passwordField: UITextField!
+  @IBOutlet var loginButton: UIButton!
+  
+  var username = Flow<String>()
+  var password = Flow<String>()
+  var loginDidTap = Flow<Void>()
+  
+  var viewModel: LoginViewModelType!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setUp()
+  }
+  
+  private func setUp() {
+    viewModel = LoginViewModel(view: self)
+   
+    viewModel.isCredentialValid.subscribeWithCache { [unowned self] enabled in
+      guard let enabled = enabled else { return }
+      self.loginButton.enabled = enabled
     }
+  }
+  
+}
+
+extension LoginViewController : LoginViewType {
+  func handleLoginSuccess() {
+    self.performSegueWithIdentifier(SecretListSegueIdentifier, sender: nil)
+  }
+  
+  func handleLoginFailure(error: NSError) {
+    let alert = UIAlertController(title: "Error!", message: error.localizedDescription, preferredStyle: .Alert)
+    alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+        
+    presentViewController(alert, animated: true, completion: nil)
+  }
 }
 
 extension LoginViewController {
-    // MARK: - Actions
-    
-    @IBAction func fieldDidChange(field: UITextField) {
-        enableLoginButton(isValidFields())
-    }
-    
-    @IBAction func login() {
-        APIManager.sharedManager.login(with: emailField.text!,
-                                       and: passwordField.text!) { (token, error) in
-                                        
-                                        if let error = error {
-                                            self.showAlertWithError(error)
-                                        } else {
-                                            self.performSegueWithIdentifier(SecretListSegueIdentifier, sender: nil)
-                                        }
-        }
-    }
-    
-    // MARK: - Internal Methods
-    
-    private func isValidFields() -> Bool {
-        return emailField.text?.isValidEmail() == true &&
-                passwordField.text?.isValidPassword() == true
-    }
-    
-    private func enableLoginButton(enabled: Bool) {
-        loginButton.enabled = enabled ? true : false
-        loginButton.alpha = enabled ? 1.0 : 0.5
-    }
-    
-    private func setupView() {
-        enableLoginButton(false)
-    }
-    
-    private func showAlertWithError(error: NSError) {
-        let alert = UIAlertController(title: "Error!", message: error.localizedDescription, preferredStyle: .Alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
-        
-        presentViewController(alert, animated: true, completion: nil)
-    }
+  
+  @IBAction func usernameTextFieldDidChanged() {
+    username.value = emailField.text
+  }
+  
+  @IBAction func passwordDidChanged() {
+    password.value = passwordField.text
+  }
+  
+  @IBAction func loginButtonDidTap() {
+    guard let username = username.value, password = password.value else { return }
+    viewModel.login(username, password: password)
+  }
+  
 }
-
-private extension String {
-    func isValidEmail() -> Bool {
-        if let _ = self.rangeOfString("[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .RegularExpressionSearch) {
-            return true
-        }
-        
-        return false
-    }
-    
-    func isValidPassword() -> Bool {
-        return self.characters.count >= MinimumPasswordCharacters
-    }
-}
-
