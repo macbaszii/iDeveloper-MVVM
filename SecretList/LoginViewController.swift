e913f5136a37e66a8ffe3db1d81a510fa91ab08f
Kiattisak Anoochitarom <macbaszii@gmail.com>
2016-06-25 13:31:52 +0700

    validation fields logic added

--- SecretList/LoginViewController.swift
+++ SecretList/LoginViewController.swift
@@ -8,18 +8,58 @@
 
 import UIKit
 
+private let MinimumPasswordCharacters = 6
+
 class LoginViewController: UIViewController {
+    @IBOutlet var emailField: UITextField!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var loginButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension LoginViewController {
+    // MARK: - Actions
+    
+    @IBAction func fieldDidChange(field: UITextField) {
+        enableLoginButton(isValidFields())
     }
+    
+    @IBAction func login() {
+        
+    }
+    
+    // MARK: - Internal Methods
+    
+    private func isValidFields() -> Bool {
+        return emailField.text?.isValidEmail() == true &&
+                passwordField.text?.isValidPassword() == true
+    }
+    
+    private func enableLoginButton(enabled: Bool) {
+        loginButton.enabled = enabled ? true : false
+        loginButton.alpha = enabled ? 1.0 : 0.5
+    }
+    
+    private func setupView() {
+        enableLoginButton(false)
+    }
+}
 
-
+private extension String {
+    func isValidEmail() -> Bool {
+        if let _ = self.rangeOfString("[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .RegularExpressionSearch) {
+            return true
+        }
+        
+        return false
+    }
+    
+    func isValidPassword() -> Bool {
+        return self.characters.count >= MinimumPasswordCharacters
+    }
 }
 
